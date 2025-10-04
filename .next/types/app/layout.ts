84e1f175f4dd96c17b3a4db4248c586cdfdbd0fb
dilzafer <85082451+dilzafer@users.noreply.github.com>
2025-10-04 00:51:09 -0400

    packages

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/dilzafersingh/Documents/GitHub/legispal/app/layout.tsx
+// File: /Users/dilzafersingh/Documents/GitHub/oversightusa/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/dilzafersingh/Documents/GitHub/legispal/app/page.tsx
+// File: /Users/dilzafersingh/Documents/GitHub/oversightusa/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
